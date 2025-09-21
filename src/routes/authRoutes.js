b7bca3df556f91965f71544796f82d83036e886c
Ryanu01 <ryangomes08090109@gmail.com>
2025-09-22 00:04:23 +0530

    Implement authentication middleware and update routes for user authentication and todos

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,7 +2,6 @@ import express, { json } from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import db from '../db.js';
-
 const router = express.Router();
 
 router.post('/register', (req, res) => {
@@ -33,7 +32,32 @@ router.post('/register', (req, res) => {
 })
 
 router.post('/login', (req, res)=>{
+    const {username, password} = req.body;
+    try {
+        const getUser = db.prepare('SELECT * FROM users WHERE username = ?');
+        const user = getUser.get(username);
 
+        if(!user) { 
+            return res.status(404).
+            send( {message: "User not found"});
+        }
+
+        const passwordIsValid = bcrypt.compareSync(password, user.password);
+        if(!passwordIsValid) {
+            return res.sendStatus(401).
+            send({message: "Invalid password"});
+        }
+        console.log(user);
+        
+        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {
+            expiresIn: '24h'
+        });
+
+        res.json({ token })
+    } catch (error) {
+        console.log(error.message);
+        res.sendStatus(503);
+    }
 })
 
 export default router;
--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -5,7 +5,9 @@ const router = express.Router();
 
 //get todo from loged in user
 router.get('/', (req, res) => {
-
+    const getTodos = db.prepare('SELECT * FROM todos WHERE user_id = ?');
+    const todos = getTodos.all(req.userId)
+    res.json(todos);
 })
 
 //create new todo
